faa60932831ab593d837083036ce31ff5911d5d1
Lado Lomidze <lado.lomidze@gmail.com>
2018-03-26 06:25:56 +0400

    docs(links): add medium and source code links

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # React and Tailwind CSS
 
+[View Demo](https://landish.github.io/react-tailwindcss-demo/) |
+[Post on Medium](https://medium.com/@Landish/how-to-use-tailwind-css-with-react-16e9d478b8b1)
+
 ## Usage:
 
 ```
--- src/App.js
+++ src/App.js
@@ -10,9 +10,22 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="text-white text-3xl">Welcome to React</h1>
         </header>
-        <p className="text-base">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="text-base pt-4">
+          <a
+            className="bg-purple-darker text-white p-3 rounded no-underline hover:bg-purple-dark mr-2"
+            href="https://medium.com/@Landish/how-to-use-tailwind-css-with-react-16e9d478b8b1"
+            target="_blank"
+          >
+            Post on Medium
+          </a>
+          <a
+            className="bg-purple-darker text-white p-3 rounded no-underline hover:bg-purple-dark ml-2"
+            href="https://github.com/Landish/react-tailwindcss-demo"
+            target="_blank"
+          >
+            Source on Github
+          </a>
+        </div>
       </div>
     );
   }
